3f3845c997e2d5d1264bd3b3a8a58f91b8bdc791
Rawat-Rahul-24 <rawatr0799@gmail.com>
2024-12-14 00:49:45 +0000

    build issues fixed

--- src/pages/split/split-card.js
+++ src/pages/split/split-card.js
@@ -9,15 +9,11 @@ import {
 import "../../styles/split-card.css";
 
 function SplitCard({ row, setRow, person, setPerson, handlers, total, setTotal, split_history, prices, setPrices, setIsSplitComplete }) {
-
-  // console.log(row, person);
-
-
   return (
     <div className="card">
       <div className="card-container">
         <div className="person-div">
-          {person.map((n, index) => {
+          {person?.map((n, index) => {
             return (
               <div key={index} className="p-heading">
                 <input type="text" placeholder={n} className="person-input" />
@@ -26,11 +22,10 @@ function SplitCard({ row, setRow, person, setPerson, handlers, total, setTotal, 
           })}
         </div>
         <div className="checkbox-row">
-          {row.map((item, key) => {
+          {row?.map((item, key) => {
             let keys = Object.keys(item);
-
             return (
-              <div className="price-selectors">
+              <div key={key} className="price-selectors">
                 {keys.map((ele, id) => {
                   if (ele > 1) {
                     return (
@@ -65,7 +60,7 @@ function SplitCard({ row, setRow, person, setPerson, handlers, total, setTotal, 
         </div>
 
         <div className="total-row">
-          {total.map((item, index) => {
+          {total?.map((item, index) => {
             if (index != 0) {
               return (
                 <div key={index} className="total-single">
